b53daa66c3b579a1b02c9ade3ff51cd4d4fc23f4
Comanche <WhollyAnointed@hotmail.com>
2013-08-14 15:06:17 -0500

    Updated with BSD license info.

--- files/js/datatables.responsive.js
+++ files/js/datatables.responsive.js
@@ -6,19 +6,17 @@
  *
  * Copyright 2013 Seen Sai Yang, all rights reserved.
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * This source file is free software, under either the GPL v2 license or a
+ * BSD style license.
+ * 
+ * This source file is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the license files for details.
+ * 
+ * You should have received a copy of the GNU General Public License and the
+ * BSD license along with this program.  These licenses are also available at:
+ *     https://raw.github.com/Comanche/datatables-responsive/master/license-gpl2.txt
+ *     https://raw.github.com/Comanche/datatables-responsive/master/license-bsd.txt
  */
 
 'use strict';
