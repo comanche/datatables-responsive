d292add489c909948922b42fcd1f07e16a9ea755
Comanche <WhollyAnointed@hotmail.com>
2014-03-17 22:21:57 -0500

    Added DataTables 2.x support.

---
+++ examples/js/dt2-dom-bootstrap3.js
@@ -0,0 +1,28 @@
+'use strict';
+
+$(document).ready(function () {
+    var responsiveHelper = undefined;
+    var breakpointDefinition = {
+        tablet: 1024,
+        phone : 480
+    };
+    var tableElement = $('#example');
+
+    $(document).ready(function () {
+        $('#example').dataTable({
+            autoWidth        : false,
+            preDrawCallback: function () {
+                // Initialize the responsive datatables helper once.
+                if (!responsiveHelper) {
+                    responsiveHelper = new ResponsiveDatatablesHelper(tableElement, breakpointDefinition);
+                }
+            },
+            rowCallback    : function (nRow) {
+                responsiveHelper.createExpandIcon(nRow);
+            },
+            drawCallback   : function (oSettings) {
+                responsiveHelper.respond();
+            }
+        });
+    });
+});
