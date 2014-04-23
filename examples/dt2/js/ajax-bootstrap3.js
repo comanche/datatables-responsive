48b60a9808c6d283e3175c2481f338e96cf6c39a
Seen Sai Yang <WhollyAnointed@hotmail.com>
2014-04-22 22:50:01 -0500

    Added ajax example.

---
+++ examples/dt2/js/ajax-bootstrap3.js
@@ -0,0 +1,27 @@
+'use strict';
+
+$(document).ready(function () {
+    var responsiveHelper = undefined;
+    var breakpointDefinition = {
+        tablet: 1024,
+        phone : 480
+    };
+    var tableElement = $('#example');
+
+    tableElement.dataTable({
+        autoWidth      : false,
+        ajax           : './arrays.txt',
+        preDrawCallback: function () {
+            // Initialize the responsive datatables helper once.
+            if (!responsiveHelper) {
+                responsiveHelper = new ResponsiveDatatablesHelper(tableElement, breakpointDefinition);
+            }
+        },
+        rowCallback    : function (nRow) {
+            responsiveHelper.createExpandIcon(nRow);
+        },
+        drawCallback   : function (oSettings) {
+            responsiveHelper.respond();
+        }
+    });
+});
